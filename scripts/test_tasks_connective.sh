0b1b865a553a02ed0c7f29879ef7a996597d6814
bingshen.wbs <bingshen.wbs@alibaba-inc.com>
2017-09-28 01:06:16 +0800

    add task tests

---
+++ scripts/test_tasks_connective.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set +x
+service=$1
+ip_list=`nslookup tasks.$service 2>&1 | awk '/^Address/{print $3}'`
+issue=0
+for ip in $ip_list; do
+  #echo $ip | cut -d '/' -f 1
+  if ! echo $ip | cut -d '/' -f 1 | xargs ping -w1 -c1 > /dev/null; then
+    issue=$(( $issue + 1 ));
+    echo `ip route get $ip | awk '/src/{print $NF}'`"->"${ip} ping have issue;
+  fi
+done
+exit $issue
